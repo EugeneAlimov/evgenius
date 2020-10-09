ba817c18f3cc6be40a2b1865157828ac44cfdf89
EugeneAlimov <zeka_ali@i.ua>
2020-10-09 23:11:20 +0200

    Сделал генерацию html с отфильтрованным списком дегов, полученным от сервера

--- static/js/AJAXscript.js
+++ static/js/AJAXscript.js
@@ -3,38 +3,26 @@
 
  //POST зарос на сервер при выборе из какой группы читать теги
 
- $(document).on('change', '#inputGroupSelect03', function (e) {           // при событии на элементе формы (изменении)
-    e.preventDefault();                                                   // сброс стандартного поведения
+ $(document).on('change', '#inputGroupSelect03', function (e) {         // при событии на элементе формы (изменении)
+    e.preventDefault();                                                 // сброс стандартного поведения
 
         let formData = $('#groupList')
         let url = formData.attr("action");
             $.ajax({
             url: url,
-            data: formData.serialize(),                                   //сериализация данных из формы
+            data: formData.serialize(),                                 //сериализация данных из формы
             type: 'POST',
-            success: function(data){
-
-            for (let key in data) {
-            let a = key
-            let b = data[key]
-            a = String(a)
-            b = String(b)
-                $(".listTags").append("<div class=\"custom-control custom-checkbox\"><div class=\"custom-control custom-checkbox\"><label class=\"custom-control-label\">" (a b) "</label></div>");
+            success: function(data){                                    // когда данные успешно вернулись разбираю data
+                for (let key in data) {                                 // на пары ключ-значение и отрисовываю список
+                    let a = key                                         // тегов с комментариями и чекбоксами
+                    let b = data[key]
+                    $(".listTags").append("<div class=\"custom-control custom-checkbox\"><div class=\"custom-control custom-checkbox\"><label class=\"custom-control-label\">" + key + data[key] + "</label></div>");
             // console.log(key, data[key])
                 }
             }
-//            error: alert('Произошла ошибка :(Обновите страницу и попробуйте снова.')
         });
 
     });
-
-
-
-
-//<div class="custom-control custom-checkbox">
-//<div class="custom-control custom-checkbox">
-//<label class="custom-control-label" for={{ tags.name_tag }}>{{ tags.name_tag }}{{ tags.comment }}</label>
-//</div>
 
 // обработка элементов, которые были динамически добавлены на страницу
 // якобы рабочая
