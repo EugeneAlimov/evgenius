ede23518a3bc1b04eb8a8e7268ddb7ae932fecf6
EugeneAlimov <zeka_ali@i.ua>
2020-10-09 07:00:00 +0200

    Сделал вывод списка тегов в выпадающем окне

--- static/js/AJAXscript.js
+++ static/js/AJAXscript.js
@@ -13,19 +13,36 @@
             data: formData.serialize(),                                   //сериализация данных из формы
             type: 'POST',
             success: function(data){
-            console.log(data)
+
+            for (let key in data) {
+            let a = key
+            let b = data[key]
+            a = String(a)
+            b = String(b)
+                $(".listTags").append("<div class=\"custom-control custom-checkbox\"><div class=\"custom-control custom-checkbox\"><label class=\"custom-control-label\">" (a b) "</label></div>");
+            // console.log(key, data[key])
+                }
             }
 //            error: alert('Произошла ошибка :(Обновите страницу и попробуйте снова.')
-        })
+        });
 
     });
 
+
+
+
+//<div class="custom-control custom-checkbox">
+//<div class="custom-control custom-checkbox">
+//<label class="custom-control-label" for={{ tags.name_tag }}>{{ tags.name_tag }}{{ tags.comment }}</label>
+//</div>
 
 // обработка элементов, которые были динамически добавлены на страницу
 // якобы рабочая
 
-$(function() {
-        $(document).on('click touchstart', '.selector', function(){
-            console.log($(this));
-        });
-    });
+
+
+//$(function() {
+//        $(document).on('click touchstart', '.selector', function(){
+//            console.log($(this));
+//        });
+//    });
