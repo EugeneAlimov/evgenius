e93236bb5579b9b9be315f715505e1624060b25e
EugeneAlimov <zeka_ali@i.ua>
2020-10-10 22:32:58 +0200

    сделал генератор шаблона списка тегов

--- static/js/AJAXscript.js
+++ static/js/AJAXscript.js
@@ -1,7 +1,5 @@
 
-
-
- //POST зарос на сервер при выборе из какой группы читать теги
+//POST зарос на сервер при выборе из какой группы читать теги
 
  $(document).on('change', '#inputGroupSelect03', function (e) {         // при событии на элементе формы (изменении)
     e.preventDefault();                                                 // сброс стандартного поведения
@@ -14,15 +12,28 @@
             type: 'POST',
             success: function(data){                                    // когда данные успешно вернулись разбираю data
                 for (let key in data) {                                 // на пары ключ-значение и отрисовываю список
-                    let a = key                                         // тегов с комментариями и чекбоксами
-                    let b = data[key]
-                    $(".listTags").append("<div class=\"custom-control custom-checkbox\"><div class=\"custom-control custom-checkbox\"><label class=\"custom-control-label\">" + key + data[key] + "</label></div>");
-            // console.log(key, data[key])
+                    $(".listTags").append(
+                    "<div class=\"custom-control custom-checkbox\">" +
+                    "<input type=\"checkbox\" class=\"custom-control-input\" id=" + key + ">" +
+                    "<label class=\"custom-control-label\" for=" + key + ">" + key + data[key] +
+                    "</label></div>"
+                    );
+                    };
                 }
-            }
-        });
+            });
+});
 
-    });
+
+
+
+
+//                                            <div class=\"custom-control custom-checkbox\">
+//                                                <input type=\"checkbox\" class=\"custom-control-input\" id={{ tags.name_tag }} >
+//                                                <label class=\"custom-control-label\" for={{ tags.name_tag }}>
+//                                                    {{ tags.name_tag }}{{ tags.comment }}</label>
+//                                            </div>
+
+
 
 // обработка элементов, которые были динамически добавлены на страницу
 // якобы рабочая
