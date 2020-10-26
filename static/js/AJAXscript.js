d8f930bb01bca323f7f7eae83bad5d59b8038117
EugeneAlimov <zeka_ali@i.ua>
2020-10-26 22:49:17 +0100

    прикрутил plotly JS

--- static/js/AJAXscript.js
+++ static/js/AJAXscript.js
@@ -94,3 +94,54 @@ function tagsListForInfluxGenerator() {
 //            console.log($(this));
 //        });
 //    });
+
+(function(){
+        let trace1 = {x: [1, 2, 3], y: [4, 5, 6], name: 'yaxis1 data', type: 'scatter'};
+
+        let trace2 = {x: [2, 3, 4], y: [40, 50, 60], name: 'yaxis2 data', yaxis: 'y2', type: 'scatter'};
+
+        let trace3 = {x: [4, 5, 6], y: [40000, 50000, 60000], name: 'yaxis3 data', yaxis: 'y3', type: 'scatter'};
+
+        let trace4 = {x: [5, 6, 7], y: [400000, 500000, 600000], name: 'yaxis4 data', yaxis: 'y4', type: 'scatter'};
+
+        let data = [trace1, trace2, trace3, trace4];
+
+        let layout = {
+            title: 'multiple y-axes example',
+            width: 1800,
+            xaxis: {domain: [0.3, 0.7]},
+            yaxis: {
+                title: 'yaxis title',
+                titlefont: {color: '#1f77b4'},
+                tickfont: {color: '#1f77b4'}
+            },
+
+        yaxis2: {
+            title: 'yaxis2 title',
+            titlefont: {color: '#ff7f0e'},
+            tickfont: {color: '#ff7f0e'},
+            anchor: 'free',
+            overlaying: 'y',
+            side: 'left',
+            position: 0.15
+        },
+        yaxis3: {
+            title: 'yaxis4 title',
+            titlefont: {color: '#d62728'},
+            tickfont: {color: '#d62728'},
+            anchor: 'x',
+            overlaying: 'y',
+            side: 'right'
+        },
+        yaxis4: {
+            title: 'yaxis5 title',
+            titlefont: {color: '#9467bd'},
+            tickfont: {color: '#9467bd'},
+            anchor: 'free',
+            overlaying: 'y',
+            side: 'right',
+            position: 0.85
+        }
+         };
+    Plotly.newPlot('chart', data, layout, {scrollZoom: true}, {editable: true});
+}());
