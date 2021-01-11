0d0b339821058ff77d4b6a5f03036cb46c670bcf
EugeneAlimov <zeka_ali@i.ua>
2021-01-11 04:33:57 +0100

    Исправил баг с парсингом эксель файла

--- analytics/views.py
+++ analytics/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from openpyxl import load_workbook
 from django.core.exceptions import ObjectDoesNotExist
 from influxdb import InfluxDBClient
-
+from fnmatch import fnmatch
 
 def pars_tags_list(request):
     """Получаем Excel файл из формы и заполняем базу тегами"""
@@ -23,7 +23,7 @@ def pars_tags_list(request):
     группе соотношением один-комногим
     Если файл с тегами не содержит нужные данные (пустые колонки или ячейки (кроме комментариев)) вывести уведомление 
     об этом"""
-    for i in range(1, ws.max_row + 1):
+    for i in range(1, ws.max_row+1):
         name_group = (ws.cell(row=i, column=6)).value  # присваиваем переменной name_group значение ячейки столбца group
         try:
             Group.objects.get(name_group=name_group)  # получаем из базы значение равное переменной name_group
@@ -33,27 +33,33 @@ def pars_tags_list(request):
         group = Group.objects.get(name_group=name_group)  # заносим в переменную объект группы для текущей записи тега
                                                             # чтобы создать связь один ко многим с таблицей Tags
         name_tag = (ws.cell(row=i, column=1)).value  # выбираем ячейки из таблицы
+        if not isinstance(name_tag, (str, int, bool, float)):
+            continue
+        address_tag = (ws.cell(row=i, column=4)).value
+        name_tag = str(name_tag)
+        address_tag = str(address_tag)
         try:
-            Tags.objects.get(name_tag=name_tag)
+            Tags.objects.get(address=address_tag)
         except ObjectDoesNotExist:
-            """"Для случая когда теги импортированы из таблицы тегов"""
             comment_tag = (ws.cell(row=i, column=5)).value
-            if (ws.cell(row=i, column=4)).value.startswith('%'):
-                if str(comment_tag).capitalize == 'Spare':
+            comment_tag = str(comment_tag)
+            """"Для случая когда теги импортированы из таблицы тегов"""
+            if address_tag.startswith('%'):
+                if comment_tag.capitalize == 'Spare':
                     continue
                 tag_table = (ws.cell(row=i, column=2)).value
                 data_type = (ws.cell(row=i, column=3)).value
-                address_tag = (ws.cell(row=i, column=4)).value
+                tag_table = str(tag_table)
+                data_type = str(data_type)
             else:
                 """"Для случая когда теги полуены из DB блоков"""
-                except_list = ['InOut', 'Input', 'Output', 'Struct', 'Static']
-                if name_tag in except_list:
-                    continue
                 data_type = (ws.cell(row=i, column=2)).value
                 tag_table = (ws.cell(row=i, column=4)).value
+                if name_tag == 'InOut' or name_tag == 'Input' or name_tag == 'Output' or name_tag == 'Spare' or name_tag == 'Static' or data_type == 'Struct':
+                    continue
                 if data_type == 'Bool':
                     x = '.DBX'
-                elif data_type == 'Real' or 'Time_Of_Day' or 'Date_And_Time':
+                elif data_type == 'Real' or data_type == 'Time_Of_Day' or data_type == 'Date_And_Time' or data_type == 'DInt' or data_type == 'IEC_TIMER':
                     x = '.DBD'
                 elif data_type == 'Int' or 'Word':
                     x = '.DBW'
